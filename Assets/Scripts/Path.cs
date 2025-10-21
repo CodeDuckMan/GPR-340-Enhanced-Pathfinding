74471298f783fdaaca929281d2befd826bccb877
Nicholas Mastrangelo <nickmasterjello13@gmail.com>
2025-10-21 14:16:45 -0400

    added one more script and using ryder now : )

---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Startpathfinding() 
+    {
+        int memeees;
+        
+        // get startingPoint (agent)
+        // get size of grid (to make goal point)
+
+
+    }
+
+    void calculateHerustic() 
+    { 
+    
+    }
+
+    void showFlowField() 
+    { 
+    
+    }
+}
